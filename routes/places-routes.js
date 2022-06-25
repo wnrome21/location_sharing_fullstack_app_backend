84384ee23a2028e702e0ae8baba830a31a05aabf
wnrome21 <nateromero001@gmail.com>
2022-06-24 20:13:08 -0700

    added the routing for the get, post, patch, and delete end points. Added functionality to each middleware for the callback functions in the requests.

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,41 +1,19 @@
 const express = require("express");
 
-const router = express.Router;
+const placesControllers = require('../controllers/places-controllers')
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    locaiton: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St., New York, NY 10001",
-    creator: "u1",
-  },
-];
+const router = express.Router();
 
-router.length("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-  if (!place) { // {place} => {place: place}
-    return res
-      .status(404)
-      .json({ message: "page not found for the provided id" });
-  }
-});
 
-router.get("/:userId", (req, res, next) => {
-    const userId = req.params.uid;
-    const user = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    })
-    if (!place) {
-        return res.status(404).json({ message: "page not found for the provided user id"})
-    }
-})
+
+router.get("/:pid", placesControllers.getPlaceById);
+
+router.get("/:userId", placesControllers.getPlaceByUserId);
+
+router.post('/', placesControllers.createPlace);
+
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace)
 
 module.exports = router;
